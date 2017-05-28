2d91ad5cf5e5f731e99931a081a64c6163fff37d
Przemysław Konik <przemyslawkonik@gmail.com>
2017-05-28 12:54:24 +0200

    repackaging

--- src/controllers/CalculatorController.java
+++ src/controllers/CalculatorController.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Font;
 
 public class CalculatorController {
     @FXML private TextField display;
